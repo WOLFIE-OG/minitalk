8cc789aec450ed9930706c3ff7d5a107ae02e4fa
WOLFIE_OG <oliverjtodd@yahoo.com>
2024-01-30 15:42:11 +0000

    Update

--- src/client/client.c
+++ src/client/client.c
@@ -6,7 +6,7 @@
 /*   By: otodd <otodd@student.42london.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 14:22:32 by otodd             #+#    #+#             */
-/*   Updated: 2024/01/29 16:16:34 by otodd            ###   ########.fr       */
+/*   Updated: 2024/01/30 15:41:58 by otodd            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -79,7 +79,7 @@ int	main(int arg_n, char **arg_a)
 		ft_printf(BRED"Not enough args to execute!"RESET);
 		exit(EXIT_FAILURE);
 	}
-	if (ft_ischeck_str(arg_a[1], ft_isdigit))
+	if (!ft_ischeck_str(arg_a[1], ft_isdigit))
 	{
 		ft_printf(BRED"Invalid PID!"RESET);
 		exit(EXIT_FAILURE);
